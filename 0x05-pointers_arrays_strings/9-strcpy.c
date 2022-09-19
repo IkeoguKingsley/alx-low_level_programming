7ccfa8c790b35cf71b5560169de5fd466649fee6
Ikeogu Kingsley <kingsleynew12@gmail.com>
2022-09-19 10:30:52 -0700

    9-strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,7 +11,7 @@ char *_strcpy(char *dest, const char *src);
 	size_t dest_len = strlen(dest);
 	size_t i;
 
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[dest_len + i] = src[i];
 	}
